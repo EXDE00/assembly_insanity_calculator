22fbd320d59f411d70fe1666467f338e8b2b1349
EXDE00 <82048594+EXDE00@users.noreply.github.com>
2021-04-07 05:04:51 +0200

    Add files via upload
    Look @ my default icon
    It has reached peak smug

--- keretprogram.c
+++ keretprogram.c
@@ -4,11 +4,18 @@
 extern int calculator(int lhs, int rhs, char op[]);
 
 int main () {
-	int lhs = 1;
-	int rhs = 4;
-	char *op = "-";
+	int lhs;
+	int rhs;
+	char *op;
 	int result;
 
+	printf("Enter the left number: ");
+	scanf("%d", &lhs);
+	printf("Enter the op: ");
+	scanf("%s", op);
+	printf("Enter the right number: ");
+	scanf("%d", &rhs);
+	printf("\nThe result is: ");
 	result = calculator(lhs, rhs, op);
 	
 	printf("%d %s %d -> %d\n", lhs, op, rhs, result);
